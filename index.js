688f6af7b5f082cc0090e87ae25c31b6880d4d08
Yogashree <yogashreesekar@gmail.com>
2023-08-11 17:01:02 +0530

    final

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const validation=express();
 const bodyparser=require('body-parser')
 const {Add,Subtract, Product, Division}=require('./demo')
 
+
+// Answer 1
 validation.use(bodyparser.json());
 validation.use(bodyparser.urlencoded({extended: true}));
-
 validation.post('/validate', function(req, res){
     let email=req.body.email;
     let password=req.body.password;
@@ -18,15 +19,36 @@ validation.post('/validate', function(req, res){
 
 })
 
+// Answer 2
 validation.get('/toupper/:name', function(req, res){
     let name=(req.params.name).trim().toUpperCase();
     if(name)
         res.send(name);
     res.send("Enter something");
 })
+
+// Answer 3
 validation.get('/calculate', function(req, res){
     res.send("Addition  :  " + Add(8,9) +"  ||  Subtraction :  " +Subtract(10,5)+"  ||   Multiplication : " +Product(3,3)+"  ||  Division : " +Division(10,2))
 });
 validation.listen('8000',function(){
     console.log('listening');
 })
+
+// Answer 4
+
+/*
+____________4=>a_________________________
+db.Reviews.find({"property_type":"House"})
+
+____________4=>b_________________________
+
+db.Reviews.find({"price":{$gt:500}},{"_id":0,"listing_url":1, "name":1, "host.host_name":1, "host.host_location":1, "reviews.reviewer_name":1, "price":1})
+____________4=>c_________________________
+
+db.Reviews.find({$and:[{'address.country':"Brazil"},{"review_scores.review_scores_rating":{$gte:9}}]})
+
+____________4=>d_________________________
+
+db.Reviews.find({price:{$gt:600,$lt:900}})
+*/
